65ad7dfd859046dc91b3be4dd7da525c4ed6a993
Amogh R <amoghpavan5363@gmail.com>
2020-07-30 09:54:35 +0530

    Added Existing Files

---
+++ src/app/home/readpost/readpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-readpost',
+  templateUrl: './readpost.component.html',
+  styleUrls: ['./readpost.component.css']
+})
+export class ReadpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
