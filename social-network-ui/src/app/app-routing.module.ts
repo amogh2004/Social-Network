1f248bdc8785015e872b0314b8951604d471ab82
Meghana-Gururaj <meghanagururaj99@gmail.com>
2020-09-03 17:27:07 +0530

    meg- changes

--- social-network-ui/src/app/app-routing.module.ts
+++ social-network-ui/src/app/app-routing.module.ts
@@ -5,25 +5,17 @@ import { SigninComponent } from './home/signin/signin.component';
 import { SignupComponent } from './home/signup/signup.component';
 import { AddpostComponent } from './home/addpost/addpost.component';
 import { UserinfoComponent} from './home/userinfo/userinfo.component';
-<<<<<<< HEAD
-import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthGuard } from './guards/auth-guard.service';
->>>>>>> b1d90bf7c04ff89801ab6a1c3c06d652fc80c972
-
+import {NotFoundComponentComponent} from './not-found-component/not-found-component.component';
 const routes: Routes = [
 {path:'',redirectTo:'home',pathMatch:'full'},
 {path:'home',component:HomeComponent},
 {path:'signin',component:SigninComponent},
 {path:'signup',component:SignupComponent},
 {path:'addpost',component:AddpostComponent},
-<<<<<<< HEAD
 {path:'userinfo',component:UserinfoComponent},
-
-=======
-{path:'userinfo',component:UserinfoComponent}
->>>>>>> b1d90bf7c04ff89801ab6a1c3c06d652fc80c972
+{path:'***',component:NotFoundComponentComponent}
 ];
 
 @NgModule({
